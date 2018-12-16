60951e731b5acfb13c1a789157932182c0aae063
Sandhiyamadhavan91 <45505891+Sandhiyamadhavan91@users.noreply.github.com>
2018-12-16 14:25:02 +0530

    Update TestRunner.java

--- src/test/java/com/cucu/runnertask/TestRunner.java
+++ src/test/java/com/cucu/runnertask/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\java\\com\\cucu\\feature\\Amazon.feature", glue ="com.cucu.stepdeftask" )
+@CucumberOptions(features="src\\test\\java\\com\\cucu\\feature\\Amazon.feature", glue ="com.cucu.stepdeftask",plugin= {"pretty","html:Reports","junit:Reports/cucumber.xml","json:Reports/cucumber.json"} )
 
 public class TestRunner {
 	
